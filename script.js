70e6e6c5ff7c51788626f59f04682c4c782afb49
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-21 18:26:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,12 +7,6 @@ class Person {
 	get name(){
 		return this.name;
 	}
-	get age(){
-		return this.age;
-	}
-	set name(name){
-		this.name = name;
-	}
 	set age(age){
 		this.age = age;
 	}
@@ -20,9 +14,8 @@ class Person {
 
 class Student extends Person {
 	constructor(name,age) {
-    super(name,age); // call the super class constructor and pass in the name parameter
+    super(name,age);
 	}
-	
 	study(){
 		console.log(`${this.name} is studying`);
 	}
@@ -36,17 +29,17 @@ class Teacher extends Person {
 		console.log(`${this.name} is teaching`);
 	}
 }
-const person = new Person("John", 25);
-console.log(person.name); // Output: John
+// const person = new Person("John", 25);
+// console.log(person.name); // Output: John
 
-person.age = 30;
-console.log(person.age); // Output: 30
+// person.age = 30;
+// console.log(person.age); // Output: 30
 
-const student = new Student("Alice", 22);
-student.study(); // Output: Alice is studying
+// const student = new Student("Alice", 22);
+// student.study(); // Output: Alice is studying
 
-const teacher = new Teacher("Bob", 40);
-teacher.teach(); // Output: Bob is teaching
+// const teacher = new Teacher("Bob", 40);
+// teacher.teach(); // Output: Bob is teaching
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
