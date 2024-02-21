178685593894e0d76b21031589408709429c3a13
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-22 00:53:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
-//complete this code
 class Person {
-	constructor(name,age){
-		this.name = name;
-		this.age = age;
+	constructor(name, age){
+		this._name = name;
+		this._age = age;
 	}
 	get name(){
-		return this.name;
+		return this._name;
 	}
 	set age(age){
-		this.age = age;
+		this._age = age;
 	}
 }
+
 class Student extends Person {
-	constructor(name,age) {
-    super(name,age);
+	constructor(name, age) {
+		super(name, age);
 	}
 	study(){
-		console.log(`${this.name} is studying`);
+		console.log(`${this._name} is studying`);
 	}
 }
+
 class Teacher extends Person {
-	constructor(name,age) {
-    super(name,age);
+	constructor(name, age) {
+		super(name, age);
 	}
 	teach(){
-		console.log(`${this.name} is teaching`);
+		console.log(`${this._name} is teaching`);
+	}
+}class Person {
+	constructor(name, age){
+		this._name = name;
+		this._age = age;
+	}
+	get name(){
+		return this._name;
+	}
+	set age(age){
+		this._age = age;
+	}
+}
+
+class Student extends Person {
+	constructor(name, age) {
+		super(name, age);
+	}
+	study(){
+		console.log(`${this._name} is studying`);
+	}
+}
+
+class Teacher extends Person {
+	constructor(name, age) {
+		super(name, age);
+	}
+	teach(){
+		console.log(`${this._name} is teaching`);
 	}
 }
 window.Person = Person;
